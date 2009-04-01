877da7a632ec3e87523df7ab123bdcd499c635d9
Artie Copeland <artie@yestech.org>
2009-03-31 23:20:40 -0700

    fixed classcast exception

--- src/main/java/org/yestech/lib/hibernate/PersistentUUID.java
+++ src/main/java/org/yestech/lib/hibernate/PersistentUUID.java
@@ -26,19 +26,25 @@ public class PersistentUUID implements EnhancedUserType {
 
     public final static PersistentUUID INSTANCE = new PersistentUUID();
 
+    public PersistentUUID() {
+    }
+
     private static final int[] SQL_TYPES = new int[]
             {
                     Types.VARCHAR,
             };
 
+    @Override
     public int[] sqlTypes() {
         return SQL_TYPES;
     }
 
+    @Override
     public Class returnedClass() {
         return UUID.class;
     }
 
+    @Override
     public boolean equals(Object x, Object y) throws HibernateException {
         if (x == y) {
             return true;
@@ -52,10 +58,12 @@ public class PersistentUUID implements EnhancedUserType {
         return uidx.equals(uidy);
     }
 
+    @Override
     public int hashCode(Object object) throws HibernateException {
         return object.hashCode();
     }
 
+    @Override
     public Object nullSafeGet(ResultSet resultSet, String[] strings, Object object) throws HibernateException, SQLException {
         return nullSafeGet(resultSet, strings[0]);
 
@@ -70,6 +78,7 @@ public class PersistentUUID implements EnhancedUserType {
         return UUID.fromString((String)uuid);
     }
 
+    @Override
     public void nullSafeSet(PreparedStatement preparedStatement, Object value, int index) throws HibernateException, SQLException {
         if (value == null) {
             Hibernate.STRING.nullSafeSet(preparedStatement, null, index);
@@ -78,38 +87,46 @@ public class PersistentUUID implements EnhancedUserType {
         }
     }
 
+    @Override
     public Object deepCopy(Object value) throws HibernateException {
         if (value == null) {
             return null;
         }
 
-        return UUID.fromString((String)value);
+        return UUID.fromString(value.toString());
     }
 
+    @Override
     public boolean isMutable() {
         return false;
     }
 
+    @Override
     public Serializable disassemble(Object value) throws HibernateException {
         return (Serializable) value;
     }
 
+    @Override
     public Object assemble(Serializable cached, Object value) throws HibernateException {
         return cached;
     }
 
+    @Override
     public Object replace(Object original, Object target, Object owner) throws HibernateException {
         return original;
     }
 
+    @Override
     public String objectToSQLString(Object object) {
         throw new UnsupportedOperationException();
     }
 
+    @Override
     public String toXMLString(Object object) {
         return object.toString();
     }
 
+    @Override
     public Object fromXMLString(String string) {
         return UUID.fromString(string);
     }
