0faf1adcf52db5a52ebebe6cbaa8e0110ba1182f
Artie Copeland <artie@yestech.org>
2009-04-16 00:22:00 -0700

    added custom ibatis type handlers

---
+++ src/main/java/org/yestech/lib/ibatis/BigIntegerTypeHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
+
+/*
+ *
+ * Author:  Artie Copeland
+ * Last Modified Date: $DateTime: $
+ */
+package org.yestech.lib.ibatis;
+
+import com.ibatis.sqlmap.client.extensions.TypeHandlerCallback;
+import com.ibatis.sqlmap.client.extensions.ResultGetter;
+import com.ibatis.sqlmap.client.extensions.ParameterSetter;
+
+import java.sql.SQLException;
+import java.sql.Types;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author Artie Copeland
+ * @version $Revision: $
+ */
+public class BigIntegerTypeHandler implements TypeHandlerCallback {
+
+    public Object getResult(ResultGetter getter) throws SQLException {
+
+        Object o = getter.getObject();
+        if (o instanceof BigDecimal) {
+            BigDecimal bd = (BigDecimal) o;
+            return bd.toBigInteger();
+        }
+        else if (o instanceof String) {
+            return new BigInteger((String) o);
+        }
+        else {
+            return o;
+        }
+    }
+
+    public void setParameter(ParameterSetter setter, Object parameter)
+            throws SQLException {
+        if (parameter == null) {
+            setter.setNull(Types.BIGINT);
+        }
+        else {
+            BigInteger i = (BigInteger) parameter;
+            setter.setBigDecimal(new BigDecimal(i));
+        }
+    }
+
+    public Object valueOf(String s) {
+        return s;
+    }
+}
---
+++ src/main/java/org/yestech/lib/ibatis/YesNoBooleanTypeHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
+
+/*
+ *
+ * Author:  Artie Copeland
+ * Last Modified Date: $DateTime: $
+ */
+package org.yestech.lib.ibatis;
+
+import com.ibatis.sqlmap.client.extensions.TypeHandlerCallback;
+import com.ibatis.sqlmap.client.extensions.ResultGetter;
+import com.ibatis.sqlmap.client.extensions.ParameterSetter;
+
+import java.sql.SQLException;
+
+/**
+ * @author Artie Copeland
+ * @version $Revision: $
+ */
+public class YesNoBooleanTypeHandler implements TypeHandlerCallback {
+    private static final String YES = "Y";
+    private static final String NO = "N";
+
+    public Object getResult(ResultGetter getter)
+            throws SQLException {
+        String s = getter.getString();
+        if (YES.equalsIgnoreCase(s)) {
+            return new Boolean(true);
+        } else if (NO.equalsIgnoreCase(s)) {
+            return new Boolean(false);
+        } else {
+            throw new SQLException(
+                    "Unexpected value " + s + " found where " + YES + " or " + NO + " was expected.");
+        }
+    }
+
+    public void setParameter(ParameterSetter setter, Object parameter)
+            throws SQLException {
+        boolean b = ((Boolean) parameter).booleanValue();
+        if (b) {
+            setter.setString(YES);
+        } else {
+            setter.setString(NO);
+        }
+    }
+
+    public Object valueOf(String s) {
+        if (YES.equalsIgnoreCase(s)) {
+            return new Boolean(true);
+        } else if (NO.equalsIgnoreCase(s)) {
+            return new Boolean(false);
+        } else {
+            throw new RuntimeException("Unexpected value " + s + " found where " + YES + " or " + NO + " was expected.");
+        }
+    }
+}
