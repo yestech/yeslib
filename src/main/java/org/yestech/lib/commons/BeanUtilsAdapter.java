5225f0c78549a30d61b2ef0781ac887b5238113e
Artie Copeland <artie@yestech.org>
2009-04-26 11:35:59 -0700

    added adapter to apache commons beanutils to make it generic

---
+++ src/main/java/org/yestech/lib/commons/BeanUtilsAdapter.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
+
+/*
+ *
+ * Author:  Artie Copeland
+ * Last Modified Date: $DateTime: $
+ */
+package org.yestech.lib.commons;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A simple adapter for bean utils the generifies and wraps the exceptions
+ * @author Artie Copeland
+ * @version $Revision: $
+ */
+@SuppressWarnings("unchecked")
+final public class BeanUtilsAdapter {
+    final private static Logger logger = LoggerFactory.getLogger(BeanUtilsAdapter.class);
+
+    public <B> B cloneBean(Object bean) {
+        try {
+            return (B)BeanUtils.cloneBean(bean);
+        } catch (Exception e) {
+            logger.error("error cloning: " + bean, e);
+            throw new RuntimeException(e);
+        }
+    }
+}
