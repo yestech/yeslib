d3381ceeceb94a868193b98348af4d806a1dbac5
Andrew Wright <ayax79@gmail.com>
2009-07-11 15:23:35 -0700

    Added a enum for candian provinces

---
+++ src/main/java/org/yestech/lib/i18n/CanadaProvinceEnum.java
@@ -0,0 +1,20 @@
+package org.yestech.lib.i18n;
+
+/**
+ * The list of all Canadian provinces.
+ */
+public enum CanadaProvinceEnum {
+    AB,
+    BC,
+    MB,
+    NB,
+    NL,
+    NT,
+    NS,
+ 	NU,
+    ON,
+    PE,
+    QC,
+    SK,
+    YT
+}
