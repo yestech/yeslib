44c6f43c4a54cb59a7a71561a28d04b1d31eab07
Andrew Wright <ayax79@gmail.com>
2009-08-15 15:31:29 -0700

    Added full name.

--- src/main/java/org/yestech/lib/i18n/CanadaProvinceEnum.java
+++ src/main/java/org/yestech/lib/i18n/CanadaProvinceEnum.java
@@ -4,17 +4,27 @@ package org.yestech.lib.i18n;
  * The list of all Canadian provinces.
  */
 public enum CanadaProvinceEnum {
-    AB,
-    BC,
-    MB,
-    NB,
-    NL,
-    NT,
-    NS,
- 	NU,
-    ON,
-    PE,
-    QC,
-    SK,
-    YT
+    ALBERTA("AB"),
+    BRITISH_COLUMBIA("BC"),
+    MANITOBA("MB"),
+    NEW_BRUNSWICK("NB"),
+    NEW_FOUNDLAND_AND_LABRADOR("NL"),
+    NORTHWEST_TERRITORIES("NT"),
+    NOVA_SCOTIA("NS"),
+ 	NUNAVUT("NU"),
+    ONTARIO("ON"),
+    PRINCE_EDWARD_ISLAND("PE"),
+    QUEBEC("QC"),
+    SASKATCHEWAN("SK"),
+    YUKON("YT");
+
+    private String code;
+
+    CanadaProvinceEnum(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
