b2478408e1d648b6be1567ad2717fdbc8847f83a
Artie Copeland <artie@yestech.org>
2009-12-19 00:29:35 -0800

    future unit test

---
+++ src/test/java/org/yestech/lib/util/AnnotationUtilUnitTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright LGPL3
+ * YES Technology Association
+ * http://yestech.org
+ *
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ */
+
+package org.yestech.lib.util;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+/**
+ *
+ *
+ */
+@Ignore
+public class AnnotationUtilUnitTest {
+    @Before
+    public void setUp() {
+        // Add your code here
+    }
+
+    @Test
+    public void testGetAnnotationFromArray() {
+        // Add your code here
+    }
+
+    @Test
+    public void testGetAnnotationFromList() {
+        // Add your code here
+    }
+}
